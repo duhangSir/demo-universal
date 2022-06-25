e3cedb2e3ca19fe5c08ae6034c15b1cae4ac9904
duhang <721197669@qq.com>
2022-06-25 19:59:43 +0800

    feat(git): 懒洋洋

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 开启ESlint检测
-  lintOnSave:true
+  lintOnSave:true,
+  devServer:{}
 })
