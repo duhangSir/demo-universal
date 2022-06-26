4a8165d3ccf850685d1a597ac67a5d29cdabfd19
duhang <721197669@qq.com>
2022-06-26 20:40:03 +0800

    chore(git): 配置svg图标

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './styles/index.scss'
+import installIcons from './icons/index'
 
 const app = createApp(App)
+installIcons(app)
 app.use(store).use(ElementPlus).use(router).mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,10 +6,26 @@
       </div>
       <el-form :rules="ruleFormRef" ref="Loginform" :model="loginForm">
         <el-form-item prop="username">
+          <span class="svg-container">
+            <el-icon>
+              <svg-icon icon="user"></svg-icon>
+            </el-icon>
+          </span>
           <el-input v-model="loginForm.username" />
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" />
+          <span class="svg-container">
+            <el-icon>
+              <svg-icon icon="password"></svg-icon>
+            </el-icon>
+          </span>
+          <el-input :type="inputType" v-model="loginForm.password" />
+
+          <span class="svg-pwd" @click="handllePassWordStatus">
+            <el-icon>
+              <svg-icon :icon="passwordIconStatus"></svg-icon>
+            </el-icon>
+          </span>
         </el-form-item>
         <el-button type="primary" @click="handleLoginButton">登录</el-button>
       </el-form>
@@ -17,11 +33,15 @@
   </div>
 </template>
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 import { validatePassword } from './rule'
 const loginForm = reactive({
   username: 'admin',
   password: '123456'
+})
+const inputType = ref('password')
+const passwordIconStatus = computed(() => {
+  return inputType.value === 'password' ? 'eye' : 'eye-open'
 })
 const Loginform = ref()
 const ruleFormRef = reactive({
@@ -35,8 +55,15 @@ const handleLoginButton = async () => {
     }
   })
 }
+const handllePassWordStatus = () => {
+  inputType.value = inputType.value === 'password' ? 'text' : 'password'
+}
 </script>
 <style scope lang="scss">
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
 .login_container {
   width: 100%;
   height: 100vh;
@@ -53,25 +80,43 @@ const handleLoginButton = async () => {
     .user_title {
       text-align: center;
       color: white;
+
       line-height: 70px;
     }
     .el-input {
       width: 200px;
-      color: white !important;
+      color: white;
+    }
+    .svg-pwd {
+      position: absolute;
+      right: 20px;
+      top: 10px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
     }
     .el-form-item__content {
       width: 600px;
       background-color: #1a2027;
+      border: 4px solid #343d47;
     }
     .el-input__wrapper {
       background: #1a2027;
       box-shadow: none;
+
       .el-input__inner {
         display: inline-block;
+        box-shadow: 0, 0, 0, 0.5;
         color: white;
       }
     }
-    //#409eff
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      display: inline-block;
+    }
     .el-button--primary {
       width: 100%;
       background-color: #358af3;
